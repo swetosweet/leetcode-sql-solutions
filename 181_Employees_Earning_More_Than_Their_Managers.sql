9b34b73d6fde91d7bae21d255950f1abe2cea6c6
Swetosree Sinha <swetosweet@gmail.com>
2019-07-11 22:37:28 -0500

    update

---
+++ 181_Employees_Earning_More_Than_Their_Managers.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+SELECT Employee.Name AS Employee FROM Employee
+JOIN Employee AS Manager ON Employee.ManagerId = Manager.Id
+WHERE Employee.Salary > Manager.Salary;
