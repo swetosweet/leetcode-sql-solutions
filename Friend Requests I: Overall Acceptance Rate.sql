cb145277626756201bd5377b7a33c35fb2038d2c
Swetosree Sinha <38987438+swetosweet@users.noreply.github.com>
2019-07-15 00:20:29 -0500

    Create Friend Requests I: Overall Acceptance Rate.sql

---
+++ Friend Requests I: Overall Acceptance Rate.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select 
+ifnull((select round(count(distinct requester_id,accepter_id)/count(distinct sender_id, send_to_id),2)
+from request_accepted, friend_request),0)
+as accept_rate;
